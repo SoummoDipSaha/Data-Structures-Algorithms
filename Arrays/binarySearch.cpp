d7aa60321377c5a46982dea3f751997eb69f65cf
Soummo Dip Saha <soummodipsaha4u9@gmail.com>
2022-05-31 15:40:47 +0530

    no changes

--- Arrays/binarySearch.cpp
+++ Arrays/binarySearch.cpp
@@ -4,8 +4,7 @@ using namespace std;
 
 int binarySearch(int *arr, int n, int k)
 {
-    int mid, up = n - 1;
-    int low = 0;
+    int mid, up = n - 1, low = 0;
     while (low <= up)
     {
         mid = (low + up) / 2;
